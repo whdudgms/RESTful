f7cd5578d1b8da39bc12b852e0ba08cce40c184e
whdudgms <108641428+whdudgms@users.noreply.github.com>
2024-03-24 11:09:32 +0900

    Spring Boot로 개발하는 RESTful Service

--- src/main/java/org/example/restful/RestfulApplication.java
+++ src/main/java/org/example/restful/RestfulApplication.java
@@ -2,12 +2,21 @@ package org.example.restful;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class RestfulApplication {
 
 	public static void main(String[] args) {
+
+		//ApplicationContext ac =
 		SpringApplication.run(RestfulApplication.class, args);
+
+//		String[] allBeanNames = ac.getBeanDefinitionNames();
+//		for(String beanName : allBeanNames){
+//			System.out.println(beanName);
+		}
 	}
 
-}
+
+
---
+++ src/main/java/org/example/restful/bean/HelloWorldBean.java
@@ -0,0 +1,15 @@
+package org.example.restful.bean;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class HelloWorldBean {
+    private final String message;
+
+//    public HelloWorldBean(String message){
+//        this.message = message;
+//    }
+}
